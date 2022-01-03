531a8456f14a22137ba2d69e9079d4070894cd5a
Catalin Spataru <catalin.spataru@cognyte.com>
2022-01-03 16:06:04 +0200

    Added base project files - configured basic Authentication via .Net Identity - added default controllers for app functionality

---
+++ AskAway/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AskAway.MvcApplication" Language="C#" %>
---
+++ AskAway/Models/Reply.cs
@@ -0,0 +1,27 @@
+﻿using AskAway.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AskAway.Models
+{
+    public class Reply
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Cotinutul raspunsului este obligatoriu")]
+        public string Content { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data este obligatorie")]
+        public DateTime Date { get; set; }
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+
+}
